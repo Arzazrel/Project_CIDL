44a619fbaa37f54433ef72ddd3a3311ffc06ee4e
Arzazrel <88975224+Arzazrel@users.noreply.github.com>
2023-06-18 16:27:24 +0200

    Add new Ifrit model and integrate in GUI program

--- Fire_cls_GUI.py
+++ Fire_cls_GUI.py
@@ -169,7 +169,7 @@ def current_view_to_visualise():
     cleanGUI()                              # clean all element of the GUI
     # create variable for the GUI elements
     explainText = "Welcom to Ifrit.\nA fire Detection CNN."
-    CNN_model_text = ['None','AlexNet','GoogleNet','Ifrit_1','Ifrit_2']
+    CNN_model_text = ['None','AlexNet','GoogleNet','Ifrit_1','Ifrit_2','Ifrit_3','Ifrit_4']
 
     # create the GUI elements and place them 
     # ---- start: explain_frame ----
@@ -755,12 +755,22 @@ def make_fit_model(chosen_model,number_epoch,num_batch_size,num_early_patience):
             return
         elif chosen_model == "Ifrit_1":                    
             Ifrit_Model = IfritNet.IfriNet(len(classes))    # create an instance of the IfriNet class
-            Ifrit_Model.make_model(1)                       # make model (IfriNet 1 architecture)
+            Ifrit_Model.make_model(3)                       # make model (IfriNet 1 architecture)
             Ifrit_Model.compile_model()                     # compile model
             network = Ifrit_Model.return_model()            # return model
         elif chosen_model == "Ifrit_2":                       
             Ifrit_Model = IfritNet.IfriNet(len(classes))    # create an instance of the IfriNet class
             Ifrit_Model.make_model(2)                       # make model (IfriNet 1 architecture)
+            Ifrit_Model.compile_model()                     # compile model
+            network = Ifrit_Model.return_model()            # return model
+        elif chosen_model == "Ifrit_3":                       
+            Ifrit_Model = IfritNet.IfriNet(len(classes))    # create an instance of the IfriNet class
+            Ifrit_Model.make_model(3)                       # make model (IfriNet 1 architecture)
+            Ifrit_Model.compile_model()                     # compile model
+            network = Ifrit_Model.return_model()            # return model
+        elif chosen_model == "Ifrit_4":                       
+            Ifrit_Model = IfritNet.IfriNet(len(classes))    # create an instance of the IfriNet class
+            Ifrit_Model.make_model(4)                       # make model (IfriNet 1 architecture)
             Ifrit_Model.compile_model()                     # compile model
             network = Ifrit_Model.return_model()            # return model
     
--- IfritNet_class.py
+++ IfritNet_class.py
@@ -101,6 +101,127 @@ class IfriNet:
             self.model.add(layers.Dropout(0.3))                                                         # dropout
             # Output layer
             self.model.add(layers.Dense(self.num_classes, activation='softmax'))
+            
+        elif version_model == 3:                    # third verstion, for more information see Note 3 at the end of the file
+            self.model = models.Sequential()
+            # 1st Conv layer
+            self.model.add(layers.Conv2D(filters=16, kernel_size=(7, 7), strides=(3,3), padding='valid', activation='relu', input_shape=(self.img_width, self.img_height, self.img_channel)))
+            self.model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2,2), padding='valid'))       # Max pooling
+            self.model.add(layers.BatchNormalization())                                                 # Batch Normalisation
+            # 2nd Conv layer
+            self.model.add(layers.Conv2D(filters=32, kernel_size=(5, 5), strides=(2,2), padding='valid', activation='relu'))
+            self.model.add(layers.BatchNormalization())                                                 # Batch Normalisation
+            # 3rd Conv layer
+            self.model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), strides=(1,1), padding='valid', activation='relu'))
+            self.model.add(layers.BatchNormalization())                                                 # Batch Normalisation
+            # 4th Conv layer
+            self.model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), strides=(1,1), padding='valid', activation='relu'))
+            self.model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(1,1), padding='valid'))       # Max pooling
+            self.model.add(layers.BatchNormalization())                                                 # Batch Normalisation
+            self.model.add(layers.Flatten())
+            # 1th dense layer
+            self.model.add(layers.Dense(64, activation='relu'))
+            self.model.add(layers.Dropout(0.3))                                                         # dropout
+            self.model.add(layers.BatchNormalization())                                                 # Batch Normalisation
+            # 2nd dense layer
+            self.model.add(layers.Dense(64, activation='relu'))
+            self.model.add(layers.Dropout(0.3))                                                         # dropout
+            # Output layer
+            self.model.add(layers.Dense(self.num_classes, activation='softmax'))
+            
+        elif version_model == 4:                    # third verstion, for more information see Note 3 at the end of the file
+
+            """
+            # vers 1
+            inp = layers.Input(shape=(self.img_width, self.img_height, self.img_channel))       # input
+            # seq_0: is the first part of the CNN network starting with the input and ending with the first auxiliary classifier
+            seq_0 = layers.Conv2D(64, 7, strides=2, padding='same', activation='relu')(inp)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = layers.Conv2D(64, 1, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.Conv2D(192, 3, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=64, fil_1x1_3x3=96, fil_3x3=128, fil_1x1_5x5=16, fil_5x5=32, fil_m_pool=32)
+            seq_0 = inception_mod(seq_0, fil_1x1=128, fil_1x1_3x3=128, fil_3x3=192, fil_1x1_5x5=32, fil_5x5=96, fil_m_pool=64)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=192, fil_1x1_3x3=96, fil_3x3=208, fil_1x1_5x5=16, fil_5x5=48, fil_m_pool=64)
+            
+            seq_2 = layers.GlobalAveragePooling2D()(seq_0)
+            seq_2 = layers.Dropout(0.4)(seq_2)
+            out = layers.Dense(self.num_classes, activation='softmax')(seq_2)           # output layer
+            """
+            """
+            # vers 2
+            inp = layers.Input(shape=(self.img_width, self.img_height, self.img_channel))       # input
+            # seq_0: is the first part of the CNN network starting with the input and ending with the first auxiliary classifier
+            seq_0 = layers.Conv2D(32, 7, strides=2, padding='same', activation='relu')(inp)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = layers.Conv2D(32, 1, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.Conv2D(64, 3, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=64, fil_1x1_3x3=96, fil_3x3=128, fil_1x1_5x5=16, fil_5x5=32, fil_m_pool=32)
+            seq_0 = inception_mod(seq_0, fil_1x1=128, fil_1x1_3x3=128, fil_3x3=192, fil_1x1_5x5=32, fil_5x5=96, fil_m_pool=64)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=192, fil_1x1_3x3=96, fil_3x3=208, fil_1x1_5x5=16, fil_5x5=48, fil_m_pool=64)
+            
+            seq_2 = layers.GlobalAveragePooling2D()(seq_0)
+            seq_2 = layers.Dropout(0.4)(seq_2)
+            out = layers.Dense(self.num_classes, activation='softmax')(seq_2)           # output layer
+            """
+            """
+            # vers 3
+            inp = layers.Input(shape=(self.img_width, self.img_height, self.img_channel))       # input
+            # seq_0: is the first part of the CNN network starting with the input and ending with the first auxiliary classifier
+            seq_0 = layers.Conv2D(32, 7, strides=2, padding='same', activation='relu')(inp)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = layers.Conv2D(32, 1, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.Conv2D(64, 3, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=32, fil_1x1_3x3=32, fil_3x3=64, fil_1x1_5x5=32, fil_5x5=32, fil_m_pool=32)
+            seq_0 = inception_mod(seq_0, fil_1x1=64, fil_1x1_3x3=64, fil_3x3=128, fil_1x1_5x5=32, fil_5x5=64, fil_m_pool=64)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=128, fil_1x1_3x3=96, fil_3x3=128, fil_1x1_5x5=64, fil_5x5=64, fil_m_pool=64)
+            
+            seq_2 = layers.GlobalAveragePooling2D()(seq_0)
+            seq_2 = layers.Dropout(0.4)(seq_2)
+            out = layers.Dense(self.num_classes, activation='softmax')(seq_2)           # output layer
+            """
+            """
+            # vers 4
+            inp = layers.Input(shape=(self.img_width, self.img_height, self.img_channel))       # input
+            # seq_0: is the first part of the CNN network starting with the input and ending with the first auxiliary classifier
+            seq_0 = layers.Conv2D(16, 7, strides=2, padding='same', activation='relu')(inp)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = layers.Conv2D(32, 1, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.Conv2D(64, 3, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=16, fil_1x1_3x3=16, fil_3x3=32, fil_1x1_5x5=16, fil_5x5=32, fil_m_pool=32)
+            seq_0 = inception_mod(seq_0, fil_1x1=32, fil_1x1_3x3=32, fil_3x3=64, fil_1x1_5x5=32, fil_5x5=64, fil_m_pool=64)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=64, fil_1x1_3x3=64, fil_3x3=128, fil_1x1_5x5=64, fil_5x5=64, fil_m_pool=64)
+            
+            seq_2 = layers.GlobalAveragePooling2D()(seq_0)
+            seq_2 = layers.Dropout(0.4)(seq_2)
+            out = layers.Dense(self.num_classes, activation='softmax')(seq_2)           # output layer
+            """
+            
+            # vers 5
+            inp = layers.Input(shape=(self.img_width, self.img_height, self.img_channel))       # input
+            # seq_0: is the first part of the CNN network starting with the input and ending with the first auxiliary classifier
+            seq_0 = layers.Conv2D(16, 7, strides=2, padding='same', activation='relu')(inp)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = layers.Conv2D(32, 1, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.Conv2D(64, 3, strides=1, padding='same', activation='relu')(seq_0)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=32, fil_1x1_3x3=8, fil_3x3=32, fil_1x1_5x5=8, fil_5x5=32, fil_m_pool=32)
+            seq_0 = inception_mod(seq_0, fil_1x1=32, fil_1x1_3x3=16, fil_3x3=64, fil_1x1_5x5=16, fil_5x5=64, fil_m_pool=64)
+            seq_0 = layers.MaxPooling2D(3, strides=2)(seq_0)
+            seq_0 = inception_mod(seq_0, fil_1x1=64, fil_1x1_3x3=32, fil_3x3=128, fil_1x1_5x5=32, fil_5x5=64, fil_m_pool=32)
+            
+            seq_2 = layers.GlobalAveragePooling2D()(seq_0)
+            seq_2 = layers.Dropout(0.4)(seq_2)
+            out = layers.Dense(self.num_classes, activation='softmax')(seq_2)           # output layer
+            
+            self.model = Model(inputs = inp, outputs = out)             # assign the CNN in model
         
         self.model.summary()
             
@@ -115,11 +236,19 @@ class IfriNet:
     def return_model(self):
         return self.model
 
+og0 = IfriNet(2)
+
+#og0.make_model(1)                       # make model (IfriNet 1 architecture)
+#og0.make_model(2)                       # make model (IfriNet 1 architecture)
+#og0.make_model(3)                       # make model (IfriNet 1 architecture) 
+og0.make_model(4)                       # make model (IfriNet 1 architecture) 
 """
 -------- Notes --------
 -- Note 1 --
  
 
 -- Note 2 --
+
+-- Note 3 --
 
 """
